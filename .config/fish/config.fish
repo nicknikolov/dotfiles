4f1dba8082e41167d9631ff9e397a5714d191340
Nick Nikolov <nicknikolov@users.noreply.github.com>
2022-07-26 11:55:11 +0100

    add fish stuff

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    source ~/Work/google-cloud-sdk/path.fish.inc
+end
+
+alias gs "git s"
+alias dot "/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+
